275823ac2692fbd2d4f5d486351b989998893d04
Siewejoaquim <natchopsiewe@gmail.com>
2026-03-12 15:14:41 +0100

    backend with auth, swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo-web')
+    .setDescription('API documentation for Todo-wen')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
