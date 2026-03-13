df36d985df0b594ffcd169a2e1fe558dc0ac15b8
Siewejoaquim <natchopsiewe@gmail.com>
2026-03-13 17:31:50 +0100

    styled the email message

--- backend/src/mail/mail.service.ts
+++ backend/src/mail/mail.service.ts
@@ -11,12 +11,39 @@ export class MailService {
       },
     });
 
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: 'Account Created',
-      text: 'Your account has been created successfully!',
-    });
+   await transporter.sendMail({
+  from: `"Todo App" <${process.env.EMAIL_USER}>`,
+  to: email,
+  subject: 'Welcome to Todo App',
+  html: `
+  <div style="font-family: Arial; padding:20px;">
+    
+    <h2>Welcome to Todo App 🎉</h2>
+    
+    <p>Your account has been created successfully.</p>
+
+    <p>Click the button below to login:</p>
+
+    <a href="http://localhost:5000/login"
+       style="
+        display:inline-block;
+        padding:12px 25px;
+        background-color:#4CAF50;
+        color:white;
+        text-decoration:none;
+        border-radius:5px;
+        font-weight:bold;
+       ">
+       Login
+    </a>
+
+    <p style="margin-top:20px;">
+      If you didn't create this account, please ignore this email.
+    </p>
+
+  </div>
+  `
+});
   }
 }
 
